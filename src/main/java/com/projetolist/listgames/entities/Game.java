3ba0b3644d8cbd356c9543618cca1c5428723c32
gabriel-andreoli <gabriandreoli@outlook.com>
2023-05-09 20:00:39 -0300

    seed db

--- src/main/java/com/projetolist/listgames/entities/Game.java
+++ src/main/java/com/projetolist/listgames/entities/Game.java
@@ -16,10 +16,12 @@ public class Game implements Serializable {
   @Column(name = "game_year")
   private Integer year;
   private String genre;
-  private String platform;
+  private String platforms;
   private Double score;
   private String imgUrl;
+  @Column(columnDefinition = "TEXT")
   private String shortDescription;
+  @Column(columnDefinition = "TEXT")
   private String longDescription;
 
   public Game(){}
@@ -29,7 +31,7 @@ public class Game implements Serializable {
     this.title = title;
     this.year = year;
     this.genre = genre;
-    this.platform = platforms;
+    this.platforms = platforms;
     this.score = score;
     this.imgUrl = imgUrl;
     this.shortDescription = shortDescription;
@@ -68,12 +70,12 @@ public class Game implements Serializable {
     this.genre = genre;
   }
 
-  public String getPlatform() {
-    return platform;
+  public String getPlatforms() {
+    return platforms;
   }
 
-  public void setPlatform(String platform) {
-    this.platform = platform;
+  public void setPlatform(String platforms) {
+    this.platforms = platforms;
   }
 
   public Double getScore() {
