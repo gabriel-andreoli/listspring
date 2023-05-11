56944c75b0cc2481a85e5b0e3e994e0a79d5c9c8
gabriel-andreoli <gabriandreoli@outlook.com>
2023-05-11 17:14:15 -0300

    game DTO and endpoint get by id

--- src/main/java/com/projetolist/listgames/services/GameService.java
+++ src/main/java/com/projetolist/listgames/services/GameService.java
@@ -1,20 +1,28 @@
 package com.projetolist.listgames.services;
 
 import com.projetolist.listgames.entities.Game;
+import com.projetolist.listgames.entities.dto.GameDTO;
 import com.projetolist.listgames.entities.dto.GameMinDTO;
 import com.projetolist.listgames.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class GameService {
   @Autowired
   private GameRepository gameRepository;
 
+  @Transactional(readOnly = true)
   public List<GameMinDTO> findAll(){
     return gameRepository.findAll().stream().map(GameMinDTO::new).toList();
+  }
+
+  @Transactional(readOnly = true)
+  public GameDTO findById(Long id){
+    Game result = gameRepository.findById(id).get();
+    return new GameDTO(result);
   }
 }
