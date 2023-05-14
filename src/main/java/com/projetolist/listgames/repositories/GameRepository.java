f74da2395e007dc425736e7d6e20fb2d7047b360
gabriel-andreoli <gabriandreoli@outlook.com>
2023-05-14 03:30:38 -0300

    homologacao ok

--- src/main/java/com/projetolist/listgames/repositories/GameRepository.java
+++ src/main/java/com/projetolist/listgames/repositories/GameRepository.java
@@ -10,12 +10,12 @@ import java.util.List;
 public interface GameRepository extends JpaRepository<Game, Long> {
 
   @Query(nativeQuery = true, value = """
-		SELECT games.id, games.title, games.game_year AS `year`, games.img_url AS imgUrl,
+		SELECT games.id, games.title, games.game_year AS gameYear, games.img_url AS imgUrl,
 		games.short_description AS shortDescription, game_belonging_list.position
 		FROM games
 		INNER JOIN game_belonging_list ON games.id = game_belonging_list.game_id
 		WHERE game_belonging_list.game_list_id = :gameListId
-		ORDER BY game_belonging_list.position
+		ORDER BY game_belonging_list.position;
 			""")
 	List<GameMinProjection> searchByList(Long gameListId);
 
